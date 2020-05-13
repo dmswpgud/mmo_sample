09a2cbb1c7220eaaa71d160736360587e8e8969d
jaehyung.eun <>
2020-05-13 20:48:00 +0900

    유틸 수정

--- MyNetwork/FreeNet_server_sourcecode/CSampleServer/GameUtils.cs
+++ MyNetwork/FreeNet_server_sourcecode/CSampleServer/GameUtils.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using GameServer;
+using Newtonsoft.Json.Linq;
 
 namespace CSampleServer
 {
@@ -96,6 +99,64 @@ namespace CSampleServer
             listResult.AddRange(defenderList);
 
             return listResult;
+        }
+    }
+}
+
+
+namespace CSampleServer
+{
+    public class SystemUtils
+    {
+        private static string path = "/Users/jaehyung.eun/Desktop/json.txt";
+
+        public static void Save(int id, JToken json)
+        {
+            var readJson = Leader();
+            
+            JToken token = null;
+
+            if (readJson != null)
+            {
+                token = readJson.SelectToken(id.ToString());    
+            }
+
+            if (token != null)
+            {
+                token.Replace(json);
+            }
+
+            if (readJson == null)
+            {
+                readJson = new JObject();
+                readJson.Add(id.ToString(), json);
+            }
+
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                sw.Write(readJson);
+            }
+        }
+
+        public static JObject Leader()
+        {
+            string strFile = path;
+
+            FileInfo fileInfo = new FileInfo(strFile);
+
+            if (fileInfo.Exists)
+            {
+                string txt;
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    txt = sr.ReadToEnd();
+                }
+
+                return JObject.Parse(txt);
+            }
+
+            return null;
         }
     }
 }
